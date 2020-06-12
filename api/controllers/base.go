366c854394e0993a108b96fc8a77bc1c294568fb
Luiz Neto <lu_u_z@hotmail.com>
2020-06-12 19:34:18 -0300

    Update base.go

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -8,10 +8,11 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 
-	_ "github.com/jinzhu/gorm/dialects/mysql" //mysql database driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" //mysql database driver
 
 	"github.com/exatasmente/go-rest/api/models"
 )
+
 
 type Server struct {
 	DB     *gorm.DB
@@ -22,17 +23,15 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 
 	var err error
 
-	if Dbdriver == "mysql" {
-		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
-		server.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database\n", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
-	}
 
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",DbHost,DbPort,DbUser,DbName,DbPassword)
+	server.DB, err = gorm.Open(Dbdriver, DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database\n", Dbdriver)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the %s database", Dbdriver)
+	}
 	server.DB.Debug().AutoMigrate(&models.User{})      //database migration
 	server.DB.Debug().AutoMigrate(&models.WpSession{}) //database migration
 	server.DB.Debug().AutoMigrate(&models.UserToken{}) //database migration
