c0ed1a579e1888fc3ef65645ac9e7996a93df6e3
exatasmente <lu_u_z@hotmail.com>
2020-06-13 15:12:42 -0300

    port

--- api/helpers/config.go
+++ api/helpers/config.go
@@ -83,7 +83,7 @@ func configLoadValues() {
 	// Server IP Value
 	Config.SetDefault("SERVER_IP", "0.0.0.0")
 	serverCfg.IP = Config.GetString("SERVER_IP")
-	serverCfg.Port = Config.GetString("SERVER_PORT")
+	serverCfg.Port = Config.GetString("PORT")
 
 	// Server Store Path Value
 	Config.SetDefault("SERVER_STORE_PATH", "./share/store")
